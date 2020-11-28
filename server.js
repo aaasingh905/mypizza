5ad82bf99bdd83a1cf7f062181f6d0da0437bc4f
Anurag Singh <aaa.singh905@gmail.com>
2020-11-28 18:49:36 +0530

    Basic HTML structure

--- server.js
+++ server.js
@@ -9,14 +9,31 @@ const path = require('path')
 
 const PORT = process.env.PORT || 3004
 
-app.get('/',(req, res)=> {
-res.render('Home')
-})
 
 app.use(expressLayout)
 
 app.set('views',path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
+
+app.use(express.static('public'))
+app.get('/',(req, res)=> {
+res.render('Home')
+})
+
+app.use(express.static('public'))
+app.get('/cart',(req, res)=> {
+res.render('customers/cart')
+})
+
+app.use(express.static('public'))
+app.get('/login',(req, res)=> {
+res.render('Auth/Login')
+})
+
+app.use(express.static('public'))
+app.get('/register',(req, res)=> {
+res.render('Auth/Register')
+})
 app.listen(PORT, () => {
     console.log('listening on port 3004')
 })
